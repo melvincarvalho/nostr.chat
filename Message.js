4a512c91b725257f611a4898e0d5bcddee4009c8
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-01-08 19:39:54 +0100

    add break-all to message

---
+++ Message.js
@@ -0,0 +1,15 @@
+import { html } from 'https://unpkg.com/htm/preact/standalone.module.js'
+
+var Message = props => {
+  return html`
+    <div class="self-${props.side === 'r' ? 'end' : 'start'} w-3/4 my-2">
+      <div
+        class="p-4 break-all text-sm bg-white rounded-t-lg rounded-${props.side}-lg shadow"
+      >
+        ${props.message.description}
+      </div>
+    </div>
+  `
+}
+
+export default Message
