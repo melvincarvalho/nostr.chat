0c055c8c552fcb4c402159468e3721514dc24337
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-01-10 20:44:19 +0100

    add decryption

--- components/Message.js
+++ components/Message.js
@@ -1,15 +1,83 @@
-import { html } from 'https://unpkg.com/htm/preact/standalone.module.js'
+import { html, Component } from 'https://unpkg.com/htm/preact/standalone.module.js'
+import secp256k1 from 'https://cdn.skypack.dev/noble-secp256k1'
+import hexToArrayBuffer from 'https://cdn.skypack.dev/hex-to-array-buffer'
 
-var Message = props => {
-  return html`
-    <div class="self-${props.side === 'r' ? 'end' : 'start'} w-3/4 my-2">
-      <div
-        class="p-4 text-sm bg-white rounded-t-lg rounded-${props.side}-lg shadow"
-      >
-        ${props.message.description}
+
+async function generateKey(rawKey) {
+  var usages = ['encrypt', 'decrypt']
+  var extractable = false
+
+  return crypto.subtle.importKey(
+    'raw',
+    rawKey,
+    { name: 'AES-CBC' },
+    extractable,
+    usages
+  )
+}
+
+function _base64ToArrayBuffer(base64) {
+  var binary_string = window.atob(base64)
+  var len = binary_string.length
+  var bytes = new Uint8Array(len)
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i)
+  }
+  return bytes.buffer
+}
+
+class Message extends Component {
+  constructor(props) {
+    super(props)
+    var id = props.pubkey === props.message.source ? props.message.destination : props.message.source
+    this.setState({ id: id })
+  }
+
+  async componentDidMount() {
+    let split = this.props.message.description?.split('?iv=')
+    console.log('chat message', split[0])
+    console.log('iv', split[1])
+    var cyphertext = split[0]
+    var iv = split[1]
+    var pub = this.state.id
+    console.log('pub', this.props.message)
+    var priv = localStorage.getItem('key')
+    this.setState({ decrypted: split[0] })
+    if (!priv) return
+    var sharedPoint = secp256k1.getSharedSecret(priv, '02' + pub)
+    var shared = sharedPoint.substr(2, 64)
+
+    console.log('shared', shared)
+
+    var cyphertext = _base64ToArrayBuffer(cyphertext)
+    var iv = _base64ToArrayBuffer(iv)
+    var genkey = await generateKey(hexToArrayBuffer(shared))
+    var decrypted = await crypto.subtle.decrypt(
+      { name: 'AES-CBC', iv: iv },
+      genkey,
+      cyphertext
+    )
+    console.log(decrypted)
+    console.log('decrypted', new TextDecoder().decode(decrypted))
+    this.setState({ decrypted: new TextDecoder().decode(decrypted) })
+
+  }
+
+  render() {
+
+
+    return html`
+      <div class="self-${this.props.side === 'r' ? 'end' : 'start'} w-3/4 my-2">
+        <div
+          class="p-4 text-sm bg-white rounded-t-lg rounded-${this.props.side}-lg shadow"
+        >
+          ${this.state.decrypted}
+        </div>
       </div>
-    </div>
-  `
+    `
+
+
+  }
 }
 
 export default Message
