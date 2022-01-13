35458370e90f0a80c64570f0c89701b6f0143050
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-01-13 18:25:19 +0100

    fix serve.js as es6 module

--- bin/serve.js
+++ bin/serve.js
@@ -1,30 +1,24 @@
 #!/usr/bin/env node
 
-const handler = require('serve-handler')
-const http = require('http')
+import handler from 'serve-handler'
+import http from 'http'
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 var port = process.env.PORT || 7788
-var root = '.'
-var options = {}
-try {
-  const configFile = process.cwd() + '/config/skill-serve.json'
-  console.log('config file', configFile)
-  const config = require(configFile)
-  console.log('config', config)
-  if (config && config.port) {
-    port = config.port
-  }
-  if (config && config.root) {
-    root = config.root
-    options.public = root
-  }
-  console.log('options', options)
-} catch (e) {
-  console.error(e)
+var root = path.join(__dirname, '..')
+
+
+const options = {
+  port: port,
+  root: root
 }
 
 const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/vercel/serve-handler#options
   return handler(request, response,
     options
   )
